07a0f2a47030995ec9738cfc744cdd527577a37f
Q <1037987714@qq.com>
2022-06-24 10:29:16 +0800

    first commit

---
+++ src/test/java/com/srq/cn/cw/CwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srq.cn.cw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
